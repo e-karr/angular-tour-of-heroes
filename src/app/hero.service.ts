9dfde0cbe22ab495cc4dbf54c2e4a97df6febaad
Elizabeth Karr <e.karr93@icloud.com>
2023-05-19 10:21:05 -0500

    refactor getHeroes method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,9 +16,7 @@ export class HeroService {
   ) {}
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
