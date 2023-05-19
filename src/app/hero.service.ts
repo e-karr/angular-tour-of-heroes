bdd6d8606804cddbf516d0e16ac23e6bb63e134a
Elizabeth Karr <e.karr93@icloud.com>
2023-05-19 10:39:21 -0500

    extend observable through pipe method and catchError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -17,7 +17,9 @@ export class HeroService {
   ) {}
 
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>('getHeroes', [])));
   }
 
   getHero(id: number): Observable<Hero> {
